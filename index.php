0afb125db15b4c249e467d7abb254563dfa72f37
ayanakkabi1 <aynour200609@gmail.com>
2025-11-27 17:17:30 +0100

    init(project): initialize PHP exercises project
    Set up the initial folder structure and add base PHP files for upcoming exercises.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+echo "Hello Aya ! Bienvenue en PHP";
+$nom="Aya";
+$age=19;
+$estEtudiant=true;
+echo "Bonjour, je suis $nom, j'ai $age et " .($estEtudiant ? "je suis Etudiante" :"je ne suis pas Etudiante" )."."
+?>
+
+
